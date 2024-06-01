3b0f9ec431ba9f4779ebf58b9fa5999a4d9b4381
ben@blueinno <ben@blueinnotechnology.com>
2024-06-01 12:08:22 +0800

    lesson 8

--- main.py
+++ main.py
@@ -9,6 +9,8 @@ HEIGHT = 768
 player = Actor("playership2_blue")
 player.pos = (WIDTH/2, HEIGHT/2)
 player.hp = 100
+player.score = 0
+
 laser_count = 0
 
 bg = Actor("space_bg")
@@ -28,9 +30,10 @@ def update():
         
         player.angle = player.angle_to(mouse_pos)-90
 
-        if random.randint(0,100)<5:
+        if random.randint(0,100)<2:     # enemy lv1
             enemy = Actor('enemygreen5')
-            
+            enemy.lv = 1
+            enemy.hp = 1
             from_dir = random.randint(0,3)
             if from_dir == 0:       #from top
                 enemy.x = random.randint(0, WIDTH)
@@ -47,6 +50,27 @@ def update():
                 
             enemy.point_towards(player)
             enemies.append(enemy)
+            
+        if random.randint(0, 100) < 1 and player.score > 100:     # enemy lv2
+            enemy = Actor('enemyred3')
+            enemy.lv = 2
+            enemy.hp = 3
+            from_dir = random.randint(0, 3)
+            if from_dir == 0:  # from top
+                enemy.x = random.randint(0, WIDTH)
+                enemy.y = 0
+            elif from_dir == 1:  # from left
+                enemy.x = 0
+                enemy.y = random.randint(0, HEIGHT)
+            elif from_dir == 2:  # from bottom
+                enemy.x = random.randint(0, WIDTH)
+                enemy.y = HEIGHT
+            elif from_dir == 3:  # from right
+                enemy.x = WIDTH
+                enemy.y = random.randint(0, HEIGHT)
+
+            enemy.point_towards(player)
+            enemies.append(enemy)
                
         for e in enemies:
             e.move_forward(2)
@@ -54,7 +78,7 @@ def update():
                 enemies.remove(e)
                 continue
             if player.colliderect(e):
-                player.hp-=5
+                player.hp-=5*e.lv
                 enemies.remove(e)
                 continue
             if random.randint(0,100)<2:
@@ -64,6 +88,7 @@ def update():
                 elasers.append(elaser)
             
         if keyboard.space:
+            sounds.sfx_laser2.play()
             laser_count += 1
             
         if laser_count >= 5:
@@ -94,14 +119,17 @@ def update():
             
         for l in lasers:
             l.move_forward(10)
-            if l.bottom < 0:
+            if l.bottom < 0 or l.top >HEIGHT or l.right <0 or l.left>WIDTH:
                 lasers.remove(l)
                 continue
             for e in enemies:
                 if l.colliderect(e):
+                    e.hp -= 1
                     lasers.remove(l)
-                    enemies.remove(e)
-                    break
+                    if e.hp <= 0:
+                        enemies.remove(e)
+                        player.score += 10
+                        break
         
         for el in elasers:
             el.move_forward(5)
@@ -113,6 +141,7 @@ def update():
                 elasers.remove(el)
                 break
     else:
+        player.score -= 100
         player.hp = 100
         
 def on_mouse_move(pos):
@@ -123,6 +152,7 @@ def draw():
     screen.clear()
     bg.draw()
     screen.draw.filled_rect(Rect((0,0), (WIDTH*player.hp/100, 20)),'green')
+    screen.draw.text(str(player.score), (10 , 30), fontsize=60)
     for l in lasers:
         l.draw()
         
